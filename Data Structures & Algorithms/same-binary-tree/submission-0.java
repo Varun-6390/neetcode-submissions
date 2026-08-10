2a26aace1667f9a69c7d974377051f6d38cc9373
Varun-6390 <artistvarunlko@gmail.com>
2026-08-10 22:55:30 +0530

    Fix formatting and remove unnecessary line

--- Data Structures & Algorithms/same-binary-tree/submission-0.java
+++ Data Structures & Algorithms/same-binary-tree/submission-0.java
@@ -20,7 +20,6 @@ class Solution {
         {
             return true;
         }
-
         if(p != null && q != null && p.val == q.val)
         {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
